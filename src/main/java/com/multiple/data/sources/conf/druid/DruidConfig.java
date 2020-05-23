51de794566429e1ecfa378123c7778eb7b41a305
lzq <576105854@qq.com>
2020-05-23 10:54:28 +0800

    springboot 多数据源 多数据源事务处理

---
+++ src/main/java/com/multiple/data/sources/conf/druid/DruidConfig.java
@@ -0,0 +1,62 @@
+package com.multiple.data.sources.conf.druid;
+
+
+import com.alibaba.druid.filter.stat.StatFilter;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import com.alibaba.druid.wall.WallConfig;
+import com.alibaba.druid.wall.WallFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author admin
+ * @author admin
+ * @ServletComponentScan // 用于扫描所有的Servlet、filter、listener
+ */
+@Configuration
+public class DruidConfig {
+
+    @Bean
+    public ServletRegistrationBean<StatViewServlet> druidServlet() {
+        ServletRegistrationBean<StatViewServlet> servletRegistrationBean = new ServletRegistrationBean<>(new StatViewServlet(), "/druid/*");
+
+        servletRegistrationBean.addInitParameter("loginUsername", "root");
+        servletRegistrationBean.addInitParameter("loginPassword", "1234");
+        return servletRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<WebStatFilter> filterRegistrationBean() {
+        FilterRegistrationBean<WebStatFilter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new WebStatFilter());
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+        filterRegistrationBean.addInitParameter("profileEnable", "true");
+        return filterRegistrationBean;
+    }
+
+    @Bean
+    public StatFilter statFilter(){
+        StatFilter statFilter = new StatFilter();
+        //slowSqlMillis用来配置SQL慢的标准，执行时间超过slowSqlMillis的就是慢。
+        statFilter.setLogSlowSql(true);
+        //SQL合并配置
+        statFilter.setMergeSql(true);
+        //slowSqlMillis的缺省值为3000，也就是3秒。
+        statFilter.setSlowSqlMillis(1000);
+        return statFilter;
+    }
+
+    @Bean
+    public WallFilter wallFilter(){
+        WallFilter wallFilter = new WallFilter();
+        //允许执行多条SQL
+        WallConfig config = new WallConfig();
+        config.setMultiStatementAllow(true);
+        wallFilter.setConfig(config);
+        return wallFilter;
+    }
+}
