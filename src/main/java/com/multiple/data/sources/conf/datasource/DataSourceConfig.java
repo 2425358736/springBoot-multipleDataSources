31157fabd79cf9494149aa3d59e83a3154a4b8b2
lzq <576105854@qq.com>
2020-05-23 10:24:36 +0800

    springboot 多数据源 多数据源事务处理

--- src/main/java/com/multiple/data/sources/conf/datasource/DataSourceConfig.java
+++ src/main/java/com/multiple/data/sources/conf/datasource/DataSourceConfig.java
@@ -2,14 +2,12 @@ package com.multiple.data.sources.conf.datasource;
 
 import org.mybatis.spring.boot.autoconfigure.MybatisProperties;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jta.atomikos.AtomikosDataSourceBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.env.Environment;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
 import java.util.Properties;
